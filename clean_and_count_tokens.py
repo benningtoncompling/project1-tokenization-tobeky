46e1ff07eb2da45371206f10a0186390479a8d80
Toby Lewkowicz <tobylewkowicz@bennington.edu>
2019-05-20 13:04:32 -0400

    Part 1 done!
    it does the things!

--- clean_and_count_tokens.py
+++ clean_and_count_tokens.py
@@ -10,20 +10,28 @@ with open(file_name, 'r') as open_book:
     with open(out_file_name, 'w') as open_out_book:
         text = open_book.read()
         text = re.sub(r'<[^(><)]+>', r'', text)
-        text = re.sub(r'\[|]|"|\*|=|{|}|-|&|;|/|\||:|_|\(|\)|\+|!', r'', text)
+        text = re.sub(r'\[|]|"|\*|=|{|}|-|&|;|/|\||:|_|\(|\)|\+|!|\s|\t', r' ', text)
         text = text.split(" ")
         wordList = {}
 
         for word in text:
+            if word == "":
+                continue
             if word not in wordList:
                 wordList[word] = 1
             elif word in wordList:
                 wordList[word] = wordList[word] + 1
 
         for word in wordList:
-            open_out_book.write(str(word) + "\t" + str(wordList[word]) + "\n")
+            wordList[word] = int(wordList[word])
 
-# Sources:
-# https://www.itworld.com/article/2784456/using-regular-expressions-to-identify-xml-tags.html
-# python documentation
-# worked with Sophia!
+        sorted_wordList = sorted(wordList.items(), key=lambda wl: wl[0])
+        best_wordList = sorted(sorted_wordList, key=lambda wl: wl[1], reverse=True)
+        for word, count in best_wordList:
+            open_out_book.write(str(word) + "\t" + str(count) + "\n")
+
+#   Sources:
+#   https://www.itworld.com/article/2784456/using-regular-expressions-to-identify-xml-tags.html
+#   python documentation
+#   https://www.geeksforgeeks.org/python-sort-python-dictionaries-by-key-or-value/ for lambda
+#   worked with Sophia!
